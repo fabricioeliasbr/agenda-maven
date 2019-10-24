1e0f5524d18433adc19d324001f31b36d5d4278a
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2019-10-24 19:58:43 -0300

    Commit.

--- src/main/webapp/database.sql
+++ src/main/webapp/database.sql
@@ -1,24 +1,26 @@
+CREATE DATABASE AGENDA;
+USE AGENDA;
 CREATE TABLE pessoas (
 	id	INT NOT	NULL AUTO_INCREMENT,
-	nome			VARCHAR(255),
+	nome VARCHAR(255),
 	data_nascimento	DATE,
-	id_endereco		INT,
-	id_contato		INT,
-	PRIMARY KEY 	(id)
+	id_endereco INT,
+	id_contato INT,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE enderecos (
 	id	INT NOT	NULL AUTO_INCREMENT,
-	logradouro		VARCHAR(255),
-	cep				VARCHAR(255),
-	PRIMARY KEY 	(id)
+	logradouro VARCHAR(255),
+	cep VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE contatos (
 	id	INT NOT	NULL AUTO_INCREMENT,
-	email			VARCHAR(255),
-	telefone		VARCHAR(255),
-	PRIMARY KEY 	(id)
+	email VARCHAR(255),
+	telefone VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 ALTER TABLE pessoas ADD CONSTRAINT id_endereco FOREIGN KEY (id_endereco) REFERENCES enderecos (id);
